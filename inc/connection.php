bbe90fe1cb89e20c07a98686c7253e9bccc21fc6
Shania-ctr <shanisazoandrianshanisazo@gmail.com>
2026-06-24 09:18:22 +0300

    fonction

--- inc/connection.php
+++ inc/connection.php
@@ -7,11 +7,10 @@ function dbconnect()
         $connect = mysqli_connect('localhost', 'root', '', 'employees');
 
         if (!$connect) {
-            // Arrête le script et affiche une erreur si la connexion échoue
             die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
         }
 
-        // Optionnel : définir l'encodage des caractères pour gérer les accents (UTF-8 recommandé)
+
         mysqli_set_charset($connect, 'utf8mb4');
     }
 
